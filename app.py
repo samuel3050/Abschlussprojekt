6b53cee6801596ed20aaa5d6dccfdda2ca88afcc
Samuel <samuel.falkner08@gmail.com>
2025-05-27 14:32:55 +0200

    v4.0

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session, url_for
+from flask import Flask, render_template, request, redirect, session
 import random
 
 app = Flask(__name__)
